c79d4b24fa98566f6b69183d12f9c78840fd7041
arnitkun <ankit.nitagartala@gmail.com>
2020-04-03 01:04:02 +0530

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(err){
+    connsole.log(error);
+  }
+  res.send('index');
 });
 
 module.exports = router;
+
+

50e1bfe3e721887a746172c796fabef1fc6ba123
arnitkun <ankit.nitagartala@gmail.com>
2020-04-03 19:20:10 +0530

    added routes, allowed CORS

--- routes/devices.js
+++ routes/devices.js
@@ -1,28 +1,38 @@
-// for lights
+// mock route for devices
 var express = require('express');
 var router = express.Router();
-
+var cors = require('cors');
 //dummy list for testing
 var devices = [0,1,1,1];
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.use(cors())
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get('/', function(req,res, next) {
+  res.send({message:"Please specify a task."});
+})
 
 router.get('/list', function(req,res, next) {
-  let number_of_devices = devices.length;
-  res.send('the number of lights:' + number_of_devices)
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send(devices);
+});
 
 router.post('/add', function(req, res, next) {
   //should display name of new device
-  let device_name = req.body;
+  let device_name = req.body.device_name;
   res.send('New Device Added: ' + device_name);
-})
+});
 
 router.post('/remove', function(req,res, next) {
   let device_to_be_removed = req.body.device;
   res.send('Removing device: ' + device_to_be_removed);
+});
+
+router.put('/task',function(req,res,next) {
+  let task = req.body.task;
+  let device = req.body.device;
+  res.send(`${device} will ${task}`);
 });
 
 module.exports = router;

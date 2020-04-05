5829923673ab1ffd22c1c0117016a1006c688d33
arnitkun <ankit.nitagartala@gmail.com>
2020-04-06 00:28:20 +0530

    module to add device

---
+++ public/javascripts/addDevice.js
@@ -0,0 +1,27 @@
+var fetch = require('node-fetch');
+
+/**
+ * 
+ * @param {string} name 
+ * Name of the device to be added, duplicate device won't be added 
+ */
+function addDevice(name) {
+    console.log("adding " + name)
+    add_device_url = 'http://localhost:3000/devices/add';
+    let reqOb = {"device_name": name}
+    return fetch(add_device_url, {method: 'POST', body: JSON.stringify(reqOb), headers: { 'Content-Type': 'application/json' } })
+    .then(res => res.json())
+    .then(body=>{
+        // console.log(body)
+        return body;
+    })
+    .catch(err => {
+        console.log("Error in getting response " + err)
+    })
+    
+    
+}
+
+module.exports = {
+    addDevice
+}
